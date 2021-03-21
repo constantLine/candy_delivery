9bd6eff858eaffd36a196262b51d176e65a43f64
Const <kabzalimov@mail.ru>
2021-03-21 15:26:39 +0500

    Create 0.1

--- models.py
+++ models.py
@@ -2,14 +2,16 @@ from app import db
 
 
 class Courier(db.Model):
-    courier_id = db.Column(db.Integer, primary_key=True)
-    courier_type = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.Integer)
     regions = db.Column(db.String(255))
     working_hours = db.Column(db.String(128))
+    orders = db.relationship('Order', backref='courier', lazy='dynamic')
 
 
 class Order(db.Model):
-    order_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     weight = db.Column(db.Float)
     region = db.Column(db.Integer)
     delivery_hours = db.Column(db.String(128))
+    courier_id = db.Column(db.Integer, db.ForeignKey('courier.id'))
